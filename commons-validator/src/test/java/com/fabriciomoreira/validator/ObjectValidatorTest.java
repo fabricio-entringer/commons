946a6186a43ae70f5a71591ebfe07ab6e00b7cfd
Fabricio Entringer Moreira <moreira@gmail.com>
2022-09-13 21:06:34 +0200

    JUnit update for 5.9.0

--- commons-validator/src/test/java/com/fabriciomoreira/validator/ObjectValidatorTest.java
+++ commons-validator/src/test/java/com/fabriciomoreira/validator/ObjectValidatorTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ObjectValidatorTest {
