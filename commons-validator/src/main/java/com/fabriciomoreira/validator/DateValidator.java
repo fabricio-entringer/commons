e75e99aadde53e5df5c2fc1b9fe776dc2cb665fc
Fabricio Entringer Moreira <moreira@gmail.com>
2022-09-25 23:06:48 +0200

    #26 - Migrating to JDK19 - maven target compiler = 17

--- commons-validator/src/main/java/com/fabriciomoreira/validator/DateValidator.java
+++ commons-validator/src/main/java/com/fabriciomoreira/validator/DateValidator.java
@@ -4,8 +4,13 @@ import java.time.LocalDate;
 
 public class DateValidator {
 
+    DateValidator() {
+
+    }
+
     /**
      * Check if the date passed by parameter is greater than now.
+     * @param date The date to be compared with the instant time (now).
      * @return true if the parameter is greater than Now().
      */
     public static boolean isGreaterThanNow(LocalDate date) {
